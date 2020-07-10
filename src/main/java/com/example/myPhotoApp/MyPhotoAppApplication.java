e988b9abef07646c5eca6e1e36bbb40c4b3eb737
Bahjat Khan <bahjat_khan2002@yahoo.com>
2020-07-10 18:23:15 -0400

    first commit

---
+++ src/main/java/com/example/myPhotoApp/MyPhotoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myPhotoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhotoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhotoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myPhotoApp/MyPhotoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myPhotoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhotoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
